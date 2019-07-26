f1e5f5bc6cffe7167ea56570f4ea3dd12013832e
cosmo <cosmo@cosmo.red>
2019-07-26 14:37:15 -0400

    Correct leak conductance

--- current_control.m
+++ current_control.m
@@ -4,18 +4,19 @@ close all
 % reproduces Fig 3c in Tim O'Leary's paper
 
 x = xolotl.examples.BurstingNeuron('prefix','prinz');
-x.AB.add('Leak','gbar',@() 0.0622/x.AB.A,'E',-50);
+x.AB.add('Leak','gbar',1e-5);
 
+x.t_end = 5e3;
 [V,Ca,M,I] = x.integrate;
-x.AB.Ca_target = mean(Ca(:,1));
+x.AB.Ca_target = x.AB.Ca_average;
     
-x.AB.NaV.add('oleary/IntegralController','tau_m',5000/x.AB.NaV.gbar);
-x.AB.CaT.add('oleary/IntegralController','tau_m',5000/x.AB.CaT.gbar);
-x.AB.CaS.add('oleary/IntegralController','tau_m',5000/x.AB.CaS.gbar);
-x.AB.ACurrent.add('oleary/IntegralController','tau_m',5000/x.AB.ACurrent.gbar);
-x.AB.KCa.add('oleary/IntegralController','tau_m',5000/x.AB.KCa.gbar);
-x.AB.Kd.add('oleary/IntegralController','tau_m',5000/x.AB.Kd.gbar);
-x.AB.HCurrent.add('oleary/IntegralController','tau_m',5000/x.AB.HCurrent.gbar);
+x.AB.NaV.add('oleary/IntegralController','tau_m',5e3/x.AB.NaV.gbar);
+x.AB.CaT.add('oleary/IntegralController','tau_m',5e3/x.AB.CaT.gbar);
+x.AB.CaS.add('oleary/IntegralController','tau_m',5e3/x.AB.CaS.gbar);
+x.AB.ACurrent.add('oleary/IntegralController','tau_m',5e3/x.AB.ACurrent.gbar);
+x.AB.KCa.add('oleary/IntegralController','tau_m',5e3/x.AB.KCa.gbar);
+x.AB.Kd.add('oleary/IntegralController','tau_m',5e3/x.AB.Kd.gbar);
+x.AB.HCurrent.add('oleary/IntegralController','tau_m',5e3/x.AB.HCurrent.gbar);
 
 x.t_end = 5e5;
 x.sim_dt = .1;
@@ -25,11 +26,10 @@ simulationcount = 100;
 conductances = zeros(simulationcount,8);
 
 for i = 1:simulationcount
-    g0 = (0.2/x.AB.A)*rand(8,1);
+    g0 = (2e-3/x.AB.A)*rand(8,1);
     x.set('*gbar',g0);
     x.set('*Controller.m',0);
-    x.AB.Leak.gbar = (0.001/x.AB.A)+((0.199/x.AB.A)*rand());
-
+    x.AB.Leak.gbar = (1e-5/x.AB.A)+((1.99e-3/x.AB.A)*rand());
     [V,Ca,M,I] = x.integrate;
 
     conductances(i,:) = x.get('*gbar');
@@ -37,7 +37,6 @@ for i = 1:simulationcount
 end
 
 conductances(:,7) = [];
-
 figure(); hold on
 plotmatrix(conductances)
 
@@ -54,7 +53,7 @@ ylabel('g (uS/mm^2)')
 
 subplot(2,1,2); hold on
 x.dt = .1;
-x.t_end = 1e3;
+x.t_end = 5e3;
 V = x.integrate;
 time = x.dt*(1:length(V))*1e-3;
 plot(time,V,'k')
